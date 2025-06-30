5964507021e64185b9cee99dd85fd4dacc922a72
devgsheep <dev.gsheep@gmail.com>
2025-06-30 18:05:52 +0900

    [docs] JSX 기초

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// css
 import "./index.css";
-import App from "./App";
+import StartPage from "./pages/StartPage";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const Hello = function () {
+  return <div>안녕하세요.</div>;
+};
+root.render(
+  <div className="wrap">
+    <StartPage></StartPage>
+  </div>,
+);
