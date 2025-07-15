b649b9eb40011e7da6576666f01a6b774c70a18f
devgsheep <dev.gsheep@gmail.com>
2025-07-15 15:50:12 +0900

    [react-router-dom] 설치 및 기초 이해

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import BgObjRed, { BgObj } from "./bg"; // .js 생략 가능
+import { Link } from "react-router-dom";
 
-function Header() {
-  // js 코드 자리
-  const title = "웹 서비스 제목";
-  const version = 0.5;
-  function say() {
-    return "하하하";
-  }
-  const isLogin = true;
-
-  // 아래는 html jsx 출력자리
+function Header({ children, company, service, setIsLogin }) {
   return (
-    <div>
-      <div style={isLogin ? BgObj : BgObjRed}>{title}</div>
-      <div>
-        버전:{version} {say()}
-      </div>
-    </div>
+    <header>
+      <div onClick={() => setIsLogin(true)}>로그인하기</div>
+      <div onClick={() => setIsLogin(false)}>로그아웃하기</div>
+      {children}
+      <Link to="/">✨ {company}로고</Link>
+      <Link to="/about">😂 {service}소개</Link>
+      <Link to="/about/mission">🙌 소개/미션</Link>
+      <Link to="/about/team">🐱‍🚀 소개/팀</Link>
+      <Link to="/service">😜 서비스</Link>
+      <Link to="/blog">💖 블로그</Link>
+      <Link to="/blog/design/100">🐱‍🏍 블로그 100번글</Link>
+      <Link to="/blog/design/detail?id=100&user=아이유">
+        ✌ 블로그 100번글 상세내용
+      </Link>
+    </header>
   );
 }
 
